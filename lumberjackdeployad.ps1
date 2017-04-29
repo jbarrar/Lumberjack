53ebd102394a52377062ad6859484312cb357cf9
jbarrar <jbarrar@hotmail.com>
2017-04-29 02:34:07 -0400

    Template build change

---
+++ lumberjackdeployad.ps1
@@ -0,0 +1,5 @@
+﻿
+Login-AzureRmAccount
+
+New-AzureRmResourceGroup -Name LumberjackGroup -Location "East US"
+New-AzureRmResourceGroupDeployment -Name LumberjackDeployment -ResourceGroupName LumberjackGroup -TemplateFile c:\projects\lumberjack\active-directory-new-domain-ha-2-dc\azuredeploy.json -adminUsername "adminlj" -adminPassword "admin" -domainName "lumberjack.com" -dnsPrefix "RVA"
